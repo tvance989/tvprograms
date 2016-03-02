b114907f73d3c6d39bb346cec6821d7fc0a0d422
Taylor Vance <decisiontoolbox@tvMBP14.local>
2016-03-01 21:16:34 -0600

    Code page

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,19 @@
 var express = require('express');
 var router = express.Router();
 
+var siteTitle = 'TV Programs';
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'TV Programs' });
+  res.render('index', { title: siteTitle });
+});
+
+router.get('/code', function(req, res, next) {
+  res.render('code', { title: siteTitle+' - Code' });
 });
 
 router.get('/music', function(req, res, next) {
-  res.render('music', { title: 'TV Programs - Music' });
+  res.render('music', { title: siteTitle+' - Music' });
 });
 
 module.exports = router;
