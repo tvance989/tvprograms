bdb9f1c761c25d9dd3d35ea240b70454a2f6525b
Taylor Vance <decisiontoolbox@tvMBP14.local>
2016-03-06 20:49:09 -0600

    Almost working gears. Just gotta make anim infinite.

--- public/js/gears.js
+++ public/js/gears.js
@@ -33,29 +33,36 @@ $(function(){
 	var paper = Raphael("gear-output", 1000, 200);
 
 	var pitchDiameter = 50; // PITCH DIAMETER (D) is the diameter of the pitch circle. In parallel shaft gears, the pitch diameters can be determined directly from the center distance and the number of teeth.
-	var numTeeth = 20; // PITCH DIAMETER (D) is the diameter of the pitch circle. In parallel shaft gears, the pitch diameters can be determined directly from the center distance and the number of teeth.
+	var numTeeth = 24; // PITCH DIAMETER (D) is the diameter of the pitch circle. In parallel shaft gears, the pitch diameters can be determined directly from the center distance and the number of teeth.
+	var pressureAngle = 20; // PRESSURE ANGLE (ø) is the angle at a pitch point between the line of pressure which is normal to the tooth surface, and the plane tangent to the pitch surface. In involute teeth, pressure angle is often described also as the angle between the line of action and the line tangent to the pitch circle. Standard pressure angles are established in connection with standard gear-tooth proportions.
 	var diametralPitch = numTeeth / pitchDiameter; // DIAMETRAL PITCH (P) is the ratio of the number of teeth to the pitch diameter.
-	var toothThickness = 1.5708 / diametralPitch; // at pitch diameter (D)
+	//var toothThickness = 1.5708 / diametralPitch; // at pitch diameter (D)
+	var toothThickness = 180 / numTeeth; // at pitch diameter (D)
 
 	var gear = paper.set();
 
 	// PITCH CIRCLE is the circle derived from a number of teeth and a specified diametral or circular pitch. Circle on which spacing or tooth profiles is established and from which the tooth proportions are constructed.
+	/*
 	var pitchCircle = paper.circle(0, 0, pitchDiameter);
-	pitchCircle.attr("stroke-dasharray", "- ");
-	//gear.push(pitchCircle);
+	pitchCircle.attr("stroke-dasharray", ". ");
+	gear.push(pitchCircle);
+	*/
 
-	//.use 20 deg pressure angle
-	var b = toothThickness - 2;
-	var B = toothThickness + 2;
-	var h = B;
+	var B = toothThickness;
+	var b = B / 2;
+	var h = B / (4*Math.tan(pressureAngle * Math.PI / 180));
+	console.log(B); console.log(h); console.log(b);
 	for(var i = 0; i < numTeeth; i++) {
-		var deg = (360 / numTeeth) * i;
+		var deg = i * 360 / numTeeth;
 		var tooth = paper.path("M"+(-B/2)+","+(-h/2)+"L"+(-b/2)+","+(h/2)+"L"+(b/2)+","+(h/2)+"L"+(B/2)+","+(-h/2)+"Z");
-		tooth.transform("r"+(i * 360 / numTeeth)+"t"+pitchDiameter+",0r-90");
+		tooth.transform("r"+deg+"t"+pitchDiameter+",0r-90");
+		tooth.attr("fill", "#777");
 		gear.push(tooth);
 	}
 
-	var baseCircle = paper.circle(0, 0, pitchDiameter - h/2);
+	var circleThickness = 5;
+	var baseCircle = paper.circle(0, 0, pitchDiameter - h/2 - circleThickness/2);
+	baseCircle.attr("stroke-width", circleThickness);
 	gear.push(baseCircle);
 
 	gear.attr("stroke", "#777");
@@ -65,41 +72,13 @@ $(function(){
 	$("#gear-input").keyup(function(){
 		paper.clear();
 
+		var dir = 1;
 		for(var i = 0; i < $(this).val().length; i++) {
-			var temp = gear.clone().transform("R"+((i/2)*360/numTeeth)+"...T"+(pitchDiameter * (2 * i + 1) + 10)+","+(pitchDiameter + 10));
+			var cx = pitchDiameter * (2 * i + 1) + 10;
+			var cy = pitchDiameter + 10;
+			var temp = gear.clone().transform("R"+((i/2)*360/numTeeth)+"...T"+cx+","+cy);
+			temp.animate({transform: "...R"+(360 * dir)+","+cx+","+cy+""}, 8000);
+			dir *= -1;
 		}
 	})
-
-
-	/*
-	var gearRadius = 42;
-	var toothLength = 10;
-	var paperPadding = toothLength * 2;
-
-	$("#gear-input").keyup(function(){
-		paper.clear();
-
-		for(var i = 0; i < $(this).val().length; i++) {
-			drawGear(
-				2 * gearRadius * (1 + i) + paperPadding + (i * 2 * toothLength),
-				gearRadius + paperPadding,
-				gearRadius
-			);
-		}
-	})
-
-	function drawGear(x, y, r) {
-		// circle
-		var circle = paper.circle(x, y, r);
-		circle.attr("stroke", "#999");
-		circle.attr("stroke-width", toothLength);
-
-		// teeth
-		var circle = paper.circle(x, y, r + toothLength);
-		circle.attr("stroke", "#999");
-		circle.attr("stroke-width", toothLength);
-		circle.attr("stroke-dasharray", ". ");
-		circle.attr("stroke-linecap", "square");
-	}
-	*/
 })
